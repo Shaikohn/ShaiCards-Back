828eb00663c28d691ddc0413a9a35b560aecbd3c
unknown <shaicokohn@gmail.com>
2023-09-15 19:46:01 -0300

    Changing some logic

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express')
-const { getUsers, getUser, googleUser, updateUserNickName } = require('../controllers/userControllers')
+const { getUser } = require('../controllers/userControllers')
 
 const router = express.Router()
 
 router.get('/user/:_id', getUser)
-router.get('/users/all', getUsers)
-router.post('/googleUser', googleUser)
-router.patch('/editUserNickName/:_id', updateUserNickName)
 
 module.exports = router 
