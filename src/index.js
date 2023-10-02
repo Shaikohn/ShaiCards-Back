4b6017a869d9d85a093bb502f25a7c3eacd0ffca
unknown <shaicokohn@gmail.com>
2023-10-02 17:48:41 -0300

    Starting implementing Socket.io

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 require('dotenv').config();
-const express = require('express')
-const mongoose = require('mongoose') ;
-const cors = require('cors') 
-const morgan = require('morgan')
-const { PORT, CONNECTION_URL } = process.env
-const routes = require('./routes/index') ; 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const { PORT, CONNECTION_URL } = process.env;
+const routes = require('./routes/index'); 
+const http = require("http");
+const { Server } = require("socket.io")
 
 const app = express();
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "DELETE"],
+    }
+})
+
+io.on("connection", (socket) => {
+    socket.on("send_card", (data) => {
+        socket.broadcast.emit("receive_card", data)
+    })
+})
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
@@ -17,7 +34,7 @@ app.use("/", routes);
 
 mongoose.set("strictQuery", false);
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .then(() => server.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
 
 module.exports = app
